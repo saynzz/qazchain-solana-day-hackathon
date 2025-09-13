4661fd51526b09e694be6e11e5f4e03411171410
Alexandr <90146266+saynzz@users.noreply.github.com>
2025-09-13 20:53:32 +0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,16 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { WalletConnectionProvider } from '../components/WalletProvider';
 import { JupiterProvider } from '@jup-ag/react-hook';
+import { Connection } from '@solana/web3.js';
+
+const connection = new Connection('https://api.devnet.solana.com');
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <WalletConnectionProvider>
-      <JupiterProvider>
+      <JupiterProvider connection={connection}>
         <Component {...pageProps} />
       </JupiterProvider>
     </WalletConnectionProvider>
   );
 }
-
